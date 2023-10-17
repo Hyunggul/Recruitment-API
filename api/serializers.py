c8d91b598113a5e4612a08109d035f8b2c96f1de
Hyunggul <hyunggul94@gmail.com>
2023-10-18 00:32:39 +0900

    Feat: Serializers 생성

---
+++ api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+import models
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Company
+        fields = '__all__'
+
+class RecruitmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Recruitment
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.User
+        fields = '__all__'
