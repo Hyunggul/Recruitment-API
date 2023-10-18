c8b30795a63fd42e9d9cf9cebe93418fceed4238
Hyunggul <hyunggul94@gmail.com>
2023-10-18 22:41:06 +0900

    Feat: 채용 공고 검색 기능 추가

--- api/views.py
+++ api/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from .models import Company, Recruitment, User
 from rest_framework.views import APIView
 from .serializers import CompanySerializer, RecruitmentSerializer, UserSerializer, RecruitmentDetailSerializer
-from rest_framework import status
+from rest_framework import status, filters
 from django.shortcuts import get_object_or_404
+from django.db.models import Q # filter에서 or를 사용하기 위해
 
 class CompanyAPI(APIView):
 
@@ -18,10 +19,24 @@ class RecruitmentAPI(APIView):
 
     #채용 공고 조회
     def get(self, request):
-        recruitments = Recruitment.objects.all()
+        search = request.GET.get('search')
+        
+        if search == None:
+            recruitments = Recruitment.objects.all()
+
+        elif search.isdigit(): #search가 숫자라면
+            search = int(search)
+            queryset = Recruitment.objects.filter( Q(recruitment_id = search) | Q(compensation = search))
+            recruitments = queryset  
+
+        else: #search가 숫자가 아니라면
+            queryset = Recruitment.objects.filter( Q(company_name = search) | Q(position = search)  
+                                                  | Q(tech_stack = search))                         
+            recruitments = queryset
+
         serializer = RecruitmentSerializer(recruitments, many=True)
         return Response(serializer.data)
-    
+
     #채용 공고 생성
     def post(self, request):
         serializer = RecruitmentSerializer(data=request.data)
